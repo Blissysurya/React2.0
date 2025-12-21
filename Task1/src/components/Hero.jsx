10fe66bc455deeb8d19d59c56c103ffe7636a7bc
Suryansh Dixit <150290655+Blissysurya@users.noreply.github.com>
2025-12-21 15:55:03 +0530

    Landing Page

---
+++ Task1/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div id='hero-parent'>
+        <h2>Unleash Your Inner Champion Today.</h2>
+        <h2> All in One Place. </h2>
+        <p>
+            Join the ultimate experience where craftsmanship meets passion.
+            Discover top-notch coaching, thrilling events, and a vibrant community.
+        </p>
+    </div>
+  )
+}
+
+export default Hero
---
+++ Task1/src/components/Nav.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div id='nav-parent'>
+
+        <div id="n-1">
+            <h2>Horizons</h2>
+        </div>
+
+        <div id="n-2">
+            <div id='n-2-1'>
+                About Us
+            </div>
+            <div id='n-2-2'>
+                Services
+            </div>
+            <div id='n-2-3'>
+                Coaches
+            </div>
+            <div id='n-2-4'>
+                Events
+            </div>
+            <div id='n-2-5'>
+                Contacts
+            </div>
+        </div>
+
+        <div id="n-3">
+            Book Now
+        </div>
+
+    </div>
+  )
+}
+
+export default Nav
